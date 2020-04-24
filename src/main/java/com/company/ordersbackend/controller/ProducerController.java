5388cc7b540e312937e3e7e725b9ff7d16c349cd
P Czech <webcoderc@gmail.com>
2020-04-24 14:53:03 +0200

    Update for producer

--- src/main/java/com/company/ordersbackend/controller/ProducerController.java
+++ src/main/java/com/company/ordersbackend/controller/ProducerController.java
@@ -3,9 +3,13 @@ package com.company.ordersbackend.controller;
 import com.company.ordersbackend.model.ProducerDTO;
 import com.company.ordersbackend.service.ProducerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -21,5 +25,16 @@ public class ProducerController {
     @ResponseStatus(HttpStatus.OK)
     public List<ProducerDTO> findAll() {
         return producerService.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<ProducerDTO> save(@RequestBody  @Valid ProducerDTO producerDTO, Errors errors){
+        Optional<ProducerDTO> result = producerService.save(producerDTO, errors);
+        return result.isPresent() ? ResponseEntity.ok(result.get()) : ResponseEntity.badRequest().build();
+    }
+
+    @PatchMapping(path="/{id}")
+    public ResponseEntity update(@PathVariable long id, @RequestBody @Valid ProducerDTO producerDTO){
+        return null;
     }
 }
--- src/test/java/com/company/ordersbackend/service/ProducerServiceTest.java
+++ src/test/java/com/company/ordersbackend/service/ProducerServiceTest.java
@@ -10,15 +10,17 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.Errors;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest()
 @RunWith(SpringRunner.class)
@@ -29,6 +31,10 @@ class ProducerServiceTest {
 
     @Mock
     private ProducerRepository producerRepository;
+
+    @Mock
+    Errors errors;
+
 
     private ProducerService producerService;
 
@@ -50,5 +56,35 @@ class ProducerServiceTest {
         //then
         assertThat(result, hasSize(producers.size()));
         assertThat(result.get(0), instanceOf(ProducerDTO.class));
+    }
+
+    @Test
+    public void saveTest(){
+        //given
+        ProducerDTO producerDTO = new ProducerDTO(0L, "XXX");
+        //when
+        when(errors.hasErrors()).thenReturn(false);
+        when(producerRepository.save(any())).thenReturn(dtoMapper.producerPOJO(producerDTO));
+        Optional<ProducerDTO> result = producerService.save(producerDTO, errors);
+        //then
+        assertTrue(result.isPresent());
+        assertEquals(result.get().getName(), producerDTO.getName());
+        verify(producerRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void updateTest(){
+        //given
+        long id = 1L;
+        ProducerDTO producerDTO = new ProducerDTO(id, "XXX");
+        //when
+        when(errors.hasErrors()).thenReturn(false);
+        when(producerRepository.existsById(any())).thenReturn(true);
+        when(producerRepository.findById(id)).thenReturn(Optional.of(dtoMapper.producerPOJO(producerDTO)));
+        boolean result = producerService.update(id, producerDTO, errors);
+
+        //then
+        assertTrue(result);
+        verify(producerRepository, times(1)).save(any());
     }
 }
