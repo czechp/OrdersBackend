40d50bdbe5e659f05403efe9b047a34fd62b3d55
P Czech <webcoderc@gmail.com>
2020-07-04 10:52:43 +0200

    Tests for save method in OrderService

--- src/main/java/com/company/ordersbackend/controller/OrderController.java
+++ src/main/java/com/company/ordersbackend/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +24,10 @@ public class OrderController {
     public ResponseEntity<OrderDTO> save(@RequestBody OrderDTO orderDTO, Errors errors, Principal principal) {
         Optional<OrderDTO> result = orderService.save(orderDTO, errors, principal.getName());
         return result.isPresent() ? ResponseEntity.ok(result.get()) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<OrderDTO>> getOrdersForUser(Principal principal){
+        return  null;
     }
 }
---
+++ src/test/java/com/company/ordersbackend/service/OrderServiceTest.java
@@ -0,0 +1,81 @@
+package com.company.ordersbackend.service;
+
+import com.company.ordersbackend.domain.AppUser;
+import com.company.ordersbackend.domain.Order;
+import com.company.ordersbackend.model.OrderDTO;
+import com.company.ordersbackend.repository.AppUserRepository;
+import com.company.ordersbackend.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.Errors;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class OrderServiceTest {
+    @Mock
+    Errors errors;
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private DTOMapper dtoMapper;
+
+    private OrderService orderService;
+
+    @BeforeEach
+    public void init() {
+        this.orderService = new OrderService(orderRepository, dtoMapper, appUserRepository);
+    }
+
+    @Test
+    public void saveTest() {
+        //given
+        OrderDTO orderDTO = new OrderDTO();
+        //when
+        when(appUserRepository.findByUsername(any())).thenReturn(Optional.of(new AppUser()));
+        when(errors.hasErrors()).thenReturn(false);
+        when(orderRepository.save(any())).thenReturn(new Order());
+        Optional<OrderDTO> result = orderService.save(orderDTO, errors, "user");
+        //then
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void saveTest_hasErrors() {
+        //given
+        OrderDTO orderDTO = new OrderDTO();
+        //when
+        when(appUserRepository.findByUsername(any())).thenReturn(Optional.of(new AppUser()));
+        when(errors.hasErrors()).thenReturn(true);
+        Optional<OrderDTO> result = orderService.save(orderDTO, errors, "user");
+        //then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void saveTest_UserDoesNotExist() {
+        //given
+        OrderDTO orderDTO = new OrderDTO();
+        //when
+        when(appUserRepository.findByUsername(any())).thenReturn(Optional.empty());
+        when(errors.hasErrors()).thenReturn(false);
+        when(orderRepository.save(any())).thenReturn(new Order());
+        Optional<OrderDTO> result = orderService.save(orderDTO, errors, "user");
+        //then
+        assertTrue(result.isEmpty());
+    }
+
+
+}
