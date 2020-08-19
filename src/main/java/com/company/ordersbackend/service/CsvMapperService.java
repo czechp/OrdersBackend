ac1dc726e9bc57b39fcce1a67c6054a994f0571a
P Czech <webcoderc@gmail.com>
2020-08-19 13:06:21 +0200

    Other UTF-8

--- src/main/java/com/company/ordersbackend/service/CsvMapperService.java
+++ src/main/java/com/company/ordersbackend/service/CsvMapperService.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,7 +46,7 @@ public class CsvMapperService {
         try {
             MappingIterator<Object> itemsList = csvMapper.readerFor(Item.class)
                     .with(csvSchema)
-                    .readValues(new InputStreamReader(multipartFile.getInputStream(), "UTF-8"));
+                    .readValues(new InputStreamReader(multipartFile.getInputStream(), StandardCharsets.UTF_8));
             List<Item> items = itemsList.readAll().stream()
                     .map(x -> (Item) x)
                     .collect(Collectors.toList());
