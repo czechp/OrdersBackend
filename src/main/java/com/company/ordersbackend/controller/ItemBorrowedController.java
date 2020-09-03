cfc10981e7dfb930934adaadffc879df40f7072a
P Czech <webcoderc@gmail.com>
2020-09-03 12:28:48 +0200

    Set CORS for ItemBorrowed controller

--- src/main/java/com/company/ordersbackend/controller/ItemBorrowedController.java
+++ src/main/java/com/company/ordersbackend/controller/ItemBorrowedController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("/api/itemBorrowed")
+@CrossOrigin("*")
 public class ItemBorrowedController {
     private ItemBorrowedService itemBorrowedService;
 
