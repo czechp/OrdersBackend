a159c21b1dcf96969a30924795bb5f00f19bd3a9
P Czech <webcoderc@gmail.com>
2020-10-19 10:11:37 +0200

    Refactor response from start controller

--- src/main/java/com/company/ordersbackend/controller/HomeController.java
+++ src/main/java/com/company/ordersbackend/controller/HomeController.java
@@ -10,7 +10,6 @@ public class HomeController {
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public String applicationStartingConfirmation() {
-        return "Application started";
+    public void applicationStartingConfirmation() {
     }
 }
